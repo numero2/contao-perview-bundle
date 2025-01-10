fced8e33f3a6bbb086ca2c44bcf61e9d6d2b2f3b
Benny Born <benny.born@numero2.de>
2025-01-10 14:37:53 +0100

    switch to new directory structure

--- src/DependencyInjection/PerviewExtension.php
+++ src/DependencyInjection/PerviewExtension.php
@@ -6,7 +6,7 @@
  * @author    Benny Born <benny.born@numero2.de>
  * @author    Michael Bösherz <michael.boesherz@numero2.de>
  * @license   Commercial
- * @copyright Copyright (c) 2022, numero2 - Agentur für digitales Marketing GbR
+ * @copyright Copyright (c) 2025, numero2 - Agentur für digitales Marketing GbR
  */
 
 
@@ -25,7 +25,7 @@ class PerviewExtension extends Extension {
 
         $loader = new YamlFileLoader(
             $container,
-            new FileLocator(__DIR__.'/../Resources/config')
+            new FileLocator(__DIR__.'/../../config')
         );
 
         $loader->load('listener.yml');
--- src/PerviewBundle.php
+++ src/PerviewBundle.php
@@ -6,7 +6,7 @@
  * @author    Benny Born <benny.born@numero2.de>
  * @author    Michael Bösherz <michael.boesherz@numero2.de>
  * @license   Commercial
- * @copyright Copyright (c) 2022, numero2 - Agentur für digitales Marketing GbR
+ * @copyright Copyright (c) 2025, numero2 - Agentur für digitales Marketing GbR
  */
 
 
@@ -17,4 +17,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PerviewBundle extends Bundle {
 
+
+    public function getPath(): string {
+
+        return \dirname(__DIR__);
+    }
 }
